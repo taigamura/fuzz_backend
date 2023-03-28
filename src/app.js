8c156eb90976aed6be0eda01121758e99b79e9cb
unknown <taigakimura97@gmail.com>
2023-03-28 22:30:20 +0900

    mongodb connection established

--- src/app.js
+++ src/app.js
@@ -1,16 +1,56 @@
-// load express module
+// =================================================================================
+// 
+// Modules
+// 
+// =================================================================================
+
 const express = require("express");
 const app = express()
+const mongoose = require("mongoose");
+
+require('dotenv').config()
+app.use(express.json());
+
+// =================================================================================
+// 
+// MongoDB
+// 
+// =================================================================================
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+
+// once the connection is open, this logs
+connection.once('open', () => {
+    console.log('MongoDB connection established succeessfully');
+});
+
+// =================================================================================
+// 
+// Current Page
+// 
+// =================================================================================
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
 
-app.listen(3000, () => {
-    console.log('server running at 3000')
-});
+// =================================================================================
+// 
+// Routes
+// 
+// =================================================================================
 
+const port = process.env.PORT || 3000;
 
 const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 app.use('/index', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`server running at: ${port}`)
+});
